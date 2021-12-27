d3c512351a02a9c9c5a9c0ecdc93d90160fbc739
vilaureu <vilaureu@users.noreply.github.com>
2021-12-27 10:30:07 +0100

    implement job shop description file parsing

---
+++ src/cfg.rs
@@ -0,0 +1,24 @@
+use crate::opt::Opt;
+
+#[derive(Debug)]
+pub(crate) struct Operation {
+    pub(crate) duration: u64,
+    pub(crate) machine: usize,
+}
+pub(crate) type Jobs = Vec<Vec<Operation>>;
+
+#[derive(Default, Debug)]
+pub(crate) struct Config {
+    pub(crate) jobs: Jobs,
+    pub(crate) machines: usize,
+    pub(crate) population: usize,
+}
+
+impl From<&Opt> for Config {
+    fn from(opt: &Opt) -> Self {
+        Self {
+            population: opt.population,
+            ..Default::default()
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,70 @@
+mod cfg;
 mod opt;
 
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    path::Path,
+};
+
+use anyhow::{bail, Context};
+use cfg::{Config, Operation};
 use opt::Opt;
 use structopt::StructOpt;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let opt = Opt::from_args();
+    let mut conf = (&opt).into();
+    parse_file(&opt.file, &mut conf)?;
 
-    println!("{:?}", opt);
+    println!("{:?}", conf);
+
+    Ok(())
+}
+
+fn parse_file(path: &Path, mut conf: &mut Config) -> anyhow::Result<()> {
+    let file = File::open(path).with_context(|| format!("failed to open {:?}", path))?;
+    let reader = BufReader::new(file);
+
+    let read_failed = || format!("failed to read from {:?}", path);
+
+    let mut lines = reader.lines();
+    match lines.next() {
+        Some(e) => {
+            e.with_context(read_failed)?;
+        }
+        None => return Ok(()),
+    }
+
+    let mut jobs = vec![];
+    let mut machines = 0;
+    for line in lines {
+        let line = line.with_context(read_failed)?;
+        let mut job = vec![];
+
+        let mut operations = line.split_whitespace();
+        while let Some(machine) = operations.next() {
+            let machine: usize = machine
+                .parse()
+                .with_context(|| format!("cannot parse '{}' as machine number", machine))?;
+
+            let duration = operations
+                .next()
+                .context("missing duration in input file")?;
+            let duration = duration
+                .parse()
+                .with_context(|| format!("cannot parse '{}' as duration", duration))?;
+            if duration == 0 {
+                bail!("duration cannot be zero");
+            }
+
+            job.push(Operation { duration, machine });
+            machines = machines.max(machine + 1);
+        }
+        jobs.push(job);
+    }
+
+    conf.jobs = jobs;
+    conf.machines = machines;
+    Ok(())
 }
